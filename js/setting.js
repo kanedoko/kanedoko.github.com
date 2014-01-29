b98d80f1be5e5a9db7ca6a038ec2367bb85c8146
zenpiro <zenpiro@gmail.com>
2014-01-29 17:35:10 +0900

    okokok

--- js/setting.js
+++ js/setting.js
@@ -18,16 +18,22 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
+  '1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/books.svg', color: '#C75746', bcolor: '#0AB971' },
+  '3': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '4': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#D33673' },
+  '5': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '6': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#EC2406' },
+  '7': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#938626' },
+  '8': { icon: 'icons/coal.svg', color: '#C75746', bcolor: '#C75746' },
+  '9': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#D33673' },
+  '10': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' },
+
+  '11': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#790586' },
+  '12': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#790586' },
+  '13': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#790586' },
+  '14': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' },
+
 };
 
 
